d7e005a8edcc6671b01bd9df4f73058ec5e4a8e3
MohamedAziz15 <mohamedazizmahmoud@gmail.com>
2023-10-04 21:02:54 +0300

    add Post

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+'''
+- title
+- author
+- content
+- image
+- publish_data
+- tags
+'''
+
+'''
+- fields
+- html wieghts
+- validation
+'''
+
+class post(models.Model):
+    title = models.CharField(max_length=100)\
+    #author = models.ForeignKey('auth.User', on_delete=models.CASCADE) 
+    content = models.TextField(max_length=50000)
+    publish_date = models.DateTimeField() 
+
+    
+
+
+
+
