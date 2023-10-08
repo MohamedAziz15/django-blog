593ff9bcfb15692d295536c6a1599bb32bf93fd6
MohamedAziz15 <mohamedazizmahmoud@gmail.com>
2023-10-08 21:48:07 +0300

    add posts

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import post
 # Create your views here.
+
+
+def post_list(request):
+    data = post.objects.all()  # orm ->> sql ->> db ->> return list(all posts)
+    return render(request,'posts.html',{'posts':data})
