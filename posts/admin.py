a83bba7b659dd4663e9940530bfe2924626745c3
MohamedAziz15 <mohamedazizmahmoud@gmail.com>
2023-10-08 20:55:55 +0300

    add admin customization pt2 (filter)

--- posts/admin.py
+++ posts/admin.py
@@ -8,6 +8,7 @@ from .models import post
 class postadmin(admin.ModelAdmin):
     list_display = ['title', 'publish_date']
     search_fields = ['title', 'content']
+    list_filter = ['publish_date', 'author']
 
 
 admin.site.register(post, postadmin)
